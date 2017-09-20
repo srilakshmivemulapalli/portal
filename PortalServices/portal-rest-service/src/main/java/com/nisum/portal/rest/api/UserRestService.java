616f29a5d0056fe8b228efc00650d37cb57f6c0b
Radhikapujari <32056787+Radhikapujari@users.noreply.github.com>
2017-09-20 18:52:24 +0530

    Users Rest Service refactred Get Method

--- PortalServices/portal-rest-service/src/main/java/com/nisum/portal/rest/api/UserRestService.java
+++ PortalServices/portal-rest-service/src/main/java/com/nisum/portal/rest/api/UserRestService.java
@@ -20,6 +20,7 @@ import com.nisum.portal.service.dto.Errors;
 import com.nisum.portal.service.dto.UserDTO;
 import com.nisum.portal.service.exception.UserServiceException;
 import com.nisum.portal.util.ExceptionConstans;
+import com.nisum.portal.util.UserConstants;
 
 @RestController
 @RequestMapping(value = "/v1/user")
@@ -58,9 +59,18 @@ public class UserRestService {
 	}
 	
 	@RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = "application/json")
-	public List<UserDTO> getUsers() throws UserServiceException {
-		return userService.getUsers();
+     public ResponseEntity<List<UserDTO>>  getUsers() throws UserServiceException {
+		//return userService.getUsers();
+		logger.info("UserRestService :: users");
+		List<UserDTO> users = userService.getUsers();
+		if (users.isEmpty()) {
+			// new ResponseEntity<List<UserDTO>>(HttpStatus.NO_CONTENT);
+			throw new UserServiceException(UserConstants.USERLISTEMPTY);
+			// You many decide to return HttpStatus.NOT_FOUND
+			}
+			return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
 	}
+	
 
 	@RequestMapping(value = "/updateUserDetails", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
 	public void updateUserDetails(@RequestBody User user) throws UserServiceException {
---
+++ PortalServices/portal-rest-service/src/main/java/com/nisum/portal/util/UserConstants.java
@@ -0,0 +1,8 @@
+package com.nisum.portal.util;
+
+public class UserConstants {
+	
+	public static final String USERLISTEMPTY="Users List Not Found";
+	public static final String UNKNOWN="Unknown Exception";
+
+}
