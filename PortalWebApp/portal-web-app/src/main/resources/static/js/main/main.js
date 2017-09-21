ab1e527f6ef2a38e278196afe0c688003bb4c1f6
swathidivya <bsdivya@nisum.com>
2017-09-21 14:25:58 +0530

    admin page changed to configurations page

--- PortalWebApp/portal-web-app/src/main/resources/static/js/main/main.js
+++
@@ -1,9 +0,0 @@
-var mainApp=angular.module('mainApp', [ 'ui.router' ])
-.config(function($stateProvider){
-	$stateProvider.state('main',{
-		url:'/main',
-		templateUrl:'js/main/main.html',
-		controller: 'mainController'
-	})
-})    
-		
