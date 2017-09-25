a9d18985d1278418ebe879a2ae2a40c4410d37db
prasanth <ykumar@nisum.com>
2017-09-25 15:21:16 +0530

    Updated by moving all the constatns to Constants.java

---
+++ PortalServices/portal-rest-service/src/main/java/com/nisum/portal/util/Constants.java
@@ -0,0 +1,32 @@
+package com.nisum.portal.util;
+
+public class Constants {
+	
+	
+	public static final String SUCCESS_MESSAGE = "Record Added Successfully";
+	public static final String SUCCESS_UPDATE_MESSAGE = "Record Updated Successfully";
+	public static final String USERLISTEMPTY="Users List Not Found";
+	
+	public static final String ERROR_MESSAGE = "Error While Creating Record";
+	public static final String UNKNOWN="Unknown Exception";
+	public static final String INTERNALSERVERERROR = "Internal Server Error";
+	
+	public static final String CATEGORY_EXISTS = "Category Already Exists";
+	public static final String CATEGORY_DELETE= " Categories deleted successfully";
+	public static final String  CATEGORY_NOT_EXIST= "Categories not Exist";
+	
+	public static final String USERUPDATED = "User updated Successfully";
+	public static final String USERDELETED = "User Deleted Successfully";
+	public static final String USERNOTEXISTS = "User doesnot exists";
+	
+	public static final String USERROLENOTEXISTS = "User role doesnot exists";
+	public static final String USERROLEUPDATED = "User role updated successfully";
+	public static final String USER_ROLE_EXISTS="User Role Exists Already";
+	public static final String USER_ROLE_ADDED="User Role Added Successfully";
+	public static final String USER_ROLE_DELETED="User Role Deleted Successfully";
+	
+	public static final String ALREADYDELETED = "User Already Deleted";
+	public static final String USERROLELISTEMPTY = "UserRole list is empty";
+	
+
+}
--- PortalServices/portal-rest-service/src/main/java/com/nisum/portal/util/ExceptionConstants.java
+++
@@ -1,7 +0,0 @@
-package com.nisum.portal.util;
-
-public class ExceptionConstants {
-
-	public static final String INTERNALSERVERERROR = "Internal Server Error";
-	
-}
