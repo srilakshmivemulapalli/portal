ff05308724f56f2e9b4218d95cbacd358c0fa6e5
prasanth <ykumar@nisum.com>
2017-09-27 11:57:27 +0530

    Updated

--- PortalWebApp/portal-web-app/src/main/resources/static/js/questions/questionReplyController.js
+++ PortalWebApp/portal-web-app/src/main/resources/static/js/questions/questionReplyController.js
@@ -11,33 +11,8 @@ questionApp
 								.then(
 										function(response) {
 											console.log(response);
-											// $scope.question=response;
-											$scope.question = {
-												"questionDetails" : {
-													"questionId" : 1,
-													"question" : "What is Java ??",
-													"description" : "What is Java all about.",
-													"createdDate" : 1505905242000,
-													"categoryName" : "1",
-													"emailId" : "ykumar@nisum.com",
-													"displayName" : null,
-													"questionRepliesCount" : null
-												},
-												"replyDetails" : [
-														{
-															"replyId" : 1,
-															"replyDescription" : "Java is platform independent",
-															"updatedDate" : 1505909054000,
-															"emailid" : "ykumar@nisum.com"
-														},
-														{
-															"replyId" : 2,
-															"replyDescription" : "Java is used for secure purpose",
-															"updatedDate" : 1505988109000,
-															"emailid" : "ykumar@nisum.com"
-														} ]
-											};
-										});
+											 $scope.question=response;
+																					});
 					}
 					$scope.getQuestionById();
 
