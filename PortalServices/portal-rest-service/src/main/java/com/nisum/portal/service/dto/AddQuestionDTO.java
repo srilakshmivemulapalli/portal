2e4ed79ffda90a684cf2495bcca0a7d46c73256e
prasanth <ykumar@nisum.com>
2017-09-27 16:28:20 +0530

    Updated

--- PortalServices/portal-rest-service/src/main/java/com/nisum/portal/service/dto/AddQuestionDTO.java
+++ PortalServices/portal-rest-service/src/main/java/com/nisum/portal/service/dto/AddQuestionDTO.java
@@ -1,5 +1,36 @@
 package com.nisum.portal.service.dto;
 
 public class AddQuestionDTO {
+	
+	private String emailId;
+	private Integer categoryId;
+	private String question;
+	private String description;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 
 }
+
