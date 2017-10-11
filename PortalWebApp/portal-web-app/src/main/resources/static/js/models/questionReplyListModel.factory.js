2280a72c79821dda468205d6c2decd9f3d6339cc
swathidivya <bsdivya@nisum.com>
2017-10-11 12:55:13 +0530

    save comment on UI for question/reply is added

--- PortalWebApp/portal-web-app/src/main/resources/static/js/models/questionReplyListModel.factory.js
+++ PortalWebApp/portal-web-app/src/main/resources/static/js/models/questionReplyListModel.factory.js
@@ -3,8 +3,8 @@ app
 				'QuestionReplyListModel',
 				[
 						"QuestionModel",
-						"CommentListModel","ReplyListModel",
-						function(questionModel, CommentListModel,ReplyListModel) {
+						"CommentListModel","ReplyListModel","CommentModel",
+						function(questionModel, CommentListModel,ReplyListModel,CommentModel) {
 							function QuestionReplyList() {
 								var replyList=ReplyListModel.newReplyListInstance();
 								var questionCommentList = CommentListModel
@@ -14,6 +14,7 @@ app
 								this.questionDetails = {};
 								this.questionDetails.questionComments=[];
 								this. replyDetails = [];
+								
 								this.addReplyListDetails = function(obj) {
 									_self. questionDetails = questionModel
 											.clone(obj.questionDetails);
@@ -33,6 +34,25 @@ app
 									}
 									_self. replyDetails=replyList.replies;
 									
+								};
+								
+								
+								this.addQuestionComment=function(obj){
+									var commentDetail=CommentModel.clone(obj);
+									_self.questionDetails.questionComments.push(commentDetail);
+								};
+								
+								
+								this.addReplyComment=function(obj,replyId){
+									var i = -1;
+
+									var commentDetail=CommentModel.clone(obj);
+									_self.replyDetails.find(function(innerReply){
+										i++;
+										if(innerReply.replyId=== replyId){
+											_self.replyDetails[i].replyComments.push(commentDetail);
+										}
+									})
 								};
 
 							}
