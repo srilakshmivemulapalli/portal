1902774d525edae1b6e546eb9a3dc541d5a365d7
sgowrisetty <sgowrisetty@nisum.com>
2017-10-24 19:51:44 +0530

    PTL#50 UI changes to blogs

--- PortalServices/portal-rest-service/src/main/java/com/nisum/portal/rest/api/BlogsRestService.java
+++ PortalServices/portal-rest-service/src/main/java/com/nisum/portal/rest/api/BlogsRestService.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.nisum.portal.service.api.BlogService;
 import com.nisum.portal.service.dto.BlogsDTO;
