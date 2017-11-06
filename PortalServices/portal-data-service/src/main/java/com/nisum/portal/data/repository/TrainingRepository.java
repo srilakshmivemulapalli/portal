742195446a80068d806a83351ec4bbe5759eaa2a
sridharsathu <32059438+ssathu@users.noreply.github.com>
2017-11-06 17:34:46 +0530

    PTL046: Create Approve Training Functionality--> display only online and classroom trainings approved by Admin

--- PortalServices/portal-data-service/src/main/java/com/nisum/portal/data/repository/TrainingRepository.java
+++ PortalServices/portal-data-service/src/main/java/com/nisum/portal/data/repository/TrainingRepository.java
@@ -13,15 +13,15 @@ import com.nisum.portal.data.domain.Trainings;
 public interface TrainingRepository extends JpaRepository<Trainings,Integer>{
 	
 	@Transactional
-    @Query(value = "select t from Trainings t where t.trainingType=:trainingType  and t.trainingStartDate>NOW() order by trainingStartDate asc")
+    @Query(value = "select t from Trainings t where t.trainingType=:trainingType  and t.trainingStartDate>NOW() and t.trainingStatus=2 order by trainingStartDate asc")
 	List<Trainings> fetchMyTrainings(@Param("trainingType") String trainingType);
 	
 	@Transactional
-	@Query(value="SELECT t from Trainings t where t.trainingId IN (SELECT tu.trainingId from TrainingToUser tu  where "+
+	@Query(value="SELECT t from Trainings t where t.trainingStatus=2 and t.trainingId IN (SELECT tu.trainingId from TrainingToUser tu  where "+
 			"tu.emailId= :emailId and tu.trainingPresence=1 )order by t.trainingStartDate desc")
     List<Trainings> fetchCompletedTrainings(@Param("emailId") String emailId);
 	
 	@Transactional
-	@Query(value="SELECT t from Trainings t where t.trainerEmailId=:trainerEmailId order by trainingStartDate asc")
+	@Query(value="SELECT t from Trainings t where t.trainerEmailId=:trainerEmailId and t.trainingStatus=2 order by trainingStartDate asc")
 	List<Trainings> getMyTrainings(@Param("trainerEmailId") String trainerEmailId);
 }
