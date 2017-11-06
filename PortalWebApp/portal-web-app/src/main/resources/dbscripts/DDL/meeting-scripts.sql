4cb36d7a2cc9e747956671207d755ae67406851a
Radhikapujari <32056787+Radhikapujari@users.noreply.github.com>
2017-11-06 15:46:17 +0530

    meetings dbscripts updated

--- PortalWebApp/portal-web-app/src/main/resources/dbscripts/DDL/meeting-scripts.sql
+++ PortalWebApp/portal-web-app/src/main/resources/dbscripts/DDL/meeting-scripts.sql
@@ -1,12 +1,10 @@
 CREATE TABLE `portal`.`BookMeetingRoom` (
  `bookMeetingRoomId` int(11) NOT NULL AUTO_INCREMENT,
  `beginTime` datetime DEFAULT NULL,
- `createdDate` datetime DEFAULT NULL,
  `emailId` varchar(255) DEFAULT NULL,
  `endTime` datetime DEFAULT NULL,
  `headCount` int(11) NOT NULL,
  `meetingTitle` varchar(255) DEFAULT NULL,
- `bookingDate` datetime DEFAULT NULL,
  `locationId` int(11) DEFAULT NULL,
  `meetingRoomId` int(11) DEFAULT NULL,
  PRIMARY KEY (`bookMeetingRoomId`),
@@ -18,13 +16,11 @@ CREATE TABLE `portal`.`BookMeetingRoom` (
 
 
 
+
 CREATE TABLE `portal`.`MeetingRoom`  (
  `meetingRoomId` int(11) NOT NULL AUTO_INCREMENT,
  `description` varchar(255) DEFAULT NULL,
  `location` int(11) NOT NULL,
-`beginTime` datetime DEFAULT NULL,
- `endTime` datetime DEFAULT NULL,
-  `startDate` datetime DEFAULT NULL,
  PRIMARY KEY (`meetingRoomId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
