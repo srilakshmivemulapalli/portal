4ba2589bfe90a5f1b2f6d1cc306a35d7b95b5ab1
RJ <RJ@RJ.local>
2017-11-14 18:26:10 +0530

    PTL081: Components Access based on user role

--- PortalWebApp/portal-web-app/src/main/resources/dbscripts/DDL/componentsRoleMapping.sql
+++ PortalWebApp/portal-web-app/src/main/resources/dbscripts/DDL/componentsRoleMapping.sql
@@ -23,22 +23,4 @@ CONSTRAINT `fk_component_id_components_access` FOREIGN KEY (`component_id`) REFE
 CONSTRAINT `fk_roleId_components_access` FOREIGN KEY (`roleId`) REFERENCES `UserRole` (`roleId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-select * from portal.Components;
-
-select * from portal.Components_Role_Mapping;
-
-select * from portal.userRole;
-
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('1', '1', '5', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('2', '1', '6', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('3', '2', '5', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('4', '2', '6', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('5', '3', '5', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('6', '3', '6', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('7', '4', '5', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('8', '4', '6', '0');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('9', '5', '5', '1');
-INSERT INTO `portal`.`Components_Role_Mapping` (`comp_role_map_id`, `component_id`, `roleId`, `is_comp_accessible`) VALUES ('10', '5', '6', '1');
-
-
 COMMIT;
